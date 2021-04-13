978098efcf2ae0ed2d518a1eeeff713891530f11
Miro Eklund <miro.eklund@semantum.fi>
2021-04-13 15:36:25 +0300

    Add second MapReducer

--- src/main/java/hadoop/WordCount.java
+++ src/main/java/hadoop/WordCount.java
@@ -1,14 +1,14 @@
 package main.java.hadoop;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.WritableComparable;
-import org.apache.hadoop.io.WritableComparator;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -17,7 +17,32 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class WordCount {
 
-	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+	public static class TotalCountMapper extends Mapper<Text, IntWritable, IntWritable, Text>{
+
+		public void map(Text key, IntWritable value, Context context) throws IOException, InterruptedException {
+			context.write(value, key);
+		}
+	}
+
+	public static class TotalCountReducer extends Reducer<IntWritable,Text,IntWritable,List<Text>> {
+		
+		private List<Text> result = new ArrayList<>();
+
+		public void reduce(IntWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+			for(Text t : values) {
+				result.add(t);
+			}
+			
+			context.write(key, result);
+		}
+	}
+	
+	/**
+	 * 
+	 * @author MiroEklund
+	 *
+	 */
+	public static class TextMapper extends Mapper<Object, Text, Text, IntWritable>{
 
 		private final static IntWritable one = new IntWritable(1);
 		private Text word = new Text();
@@ -31,7 +56,12 @@ public class WordCount {
 		}
 	}
 
-	public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+	/**
+	 * 
+	 * @author MiroEklund
+	 *
+	 */
+	public static class CountReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
 		
 		private IntWritable result = new IntWritable();
 
@@ -44,47 +74,89 @@ public class WordCount {
 			context.write(key, result);
 		}
 	}
-
-	public static class OccurenceComparator extends WritableComparator {
+	
+	/**
+	 * 
+	 * @param conf
+	 * @param input
+	 * @param output
+	 * @return
+	 * @throws IOException
+	 */
+	private static Job createSortByValueJob(Configuration conf, String input, String output) throws IOException {
+		Job job = Job.getInstance(conf, "count sort");
+		job.setJarByClass(WordCount.class);
+		job.setMapperClass(TotalCountMapper.class);
+		job.setReducerClass(TotalCountReducer.class);
 		
-	    protected OccurenceComparator() {
-	        super(IntWritable.class, true);
-	    }
-	    
-	    @SuppressWarnings("rawtypes")
-	    @Override
-	    public int compare(WritableComparable w1, WritableComparable w2) {
-	    	IntWritable k1 = (IntWritable)w1;
-	    	IntWritable k2 = (IntWritable)w2;
-	        return k1.compareTo(k2);
-	    }
+		// Swapped order compared to wordCountJob!
+		job.setOutputKeyClass(IntWritable.class);
+		job.setOutputValueClass(Text.class);
 		
+		FileInputFormat.addInputPath(job, new Path(input));
+		FileOutputFormat.setOutputPath(job, new Path(output));
+		
+		return job;
 	}
 	
-	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
-		
+	/**
+	 * 
+	 * @param conf
+	 * @param input
+	 * @param output
+	 * @return
+	 * @throws IOException
+	 */
+	private static Job createWordCountJob(Configuration conf, String input, String output) throws IOException {
 		Job job = Job.getInstance(conf, "word count");
 		job.setJarByClass(WordCount.class);
-		
-		// Possibly sorts by value instead of text:
-		job.setGroupingComparatorClass(OccurenceComparator.class);
-		
-		job.setMapperClass(TokenizerMapper.class);
-		
-		//job.setCombinerClass(IntSumReducer.class);
-		job.setReducerClass(IntSumReducer.class);
+		job.setMapperClass(TextMapper.class);
+		job.setReducerClass(CountReducer.class);
 		
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 		
-		if(args.length == 2) {
-			FileInputFormat.addInputPath(job, new Path(args[0]));
-			FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		FileInputFormat.addInputPath(job, new Path(input));
+		FileOutputFormat.setOutputPath(job, new Path(output));
+		
+		return job;
+	}
+	
+	/**
+	 * 
+	 * @param args
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		String input;
+		String output;
+		String output2;
+		
+		if(args.length == 3) {
+			input = args[0];
+			output = args[1];
+			output2 = args[2];
 		} else {
-			FileInputFormat.addInputPath(job, new Path(args[1]));
-			FileOutputFormat.setOutputPath(job, new Path(args[2]));	
+			input = args[1];
+			output = args[2];
+			output2 = args[3];
 		}
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
+		
+		Configuration conf = new Configuration();
+		
+		// Let's first run a MapReduce job that just counts the words. Key: word, Value: count
+		Job word_count_job = createWordCountJob(conf, input, output);
+		
+		boolean first_job_ok = word_count_job.waitForCompletion(true);
+		if(first_job_ok) {
+			
+			// Let's then run another job 
+			Job sort_by_value_job = createSortByValueJob(conf, output, output2);
+			
+			boolean second_job_ok = sort_by_value_job.waitForCompletion(true);
+			System.exit(second_job_ok ? 0 : 1);
+		} else {
+			System.exit(1);
+		}
 	}
 }
