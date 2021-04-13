7fe3fbc4b9c44fec26388a08ab856c67f340d113
Miro Eklund <miro.eklund@semantum.fi>
2021-04-13 16:59:27 +0300

    v2

--- src/main/java/hadoop/WordCount.java
+++ src/main/java/hadoop/WordCount.java
@@ -28,7 +28,7 @@ public class WordCount {
 	public static class TotalCountMapper extends Mapper<Object, Text, IntWritable, Text>{
 
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-			String[] text_count_pair = value.toString().split(","); //No idea what value we get here ...
+			String[] text_count_pair = value.toString().split("\t"); //No idea what value we get here ...
 			String text = text_count_pair[0];
 			int count = Integer.parseInt(text_count_pair[1].trim().strip());
 			IntWritable i = new IntWritable(count);
@@ -75,7 +75,7 @@ public class WordCount {
 	 * @author MiroEklund
 	 *
 	 */
-	public static class CountReducer extends Reducer<Text,IntWritable,Text,Text> {
+	public static class CountReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
 		
 		private IntWritable result = new IntWritable();
 
@@ -85,7 +85,7 @@ public class WordCount {
 				sum += val.get();
 			}
 			result.set(sum);
-			context.write(key, new Text(key + "," + result.toString()));
+			context.write(key, result);
 		}
 	}
 	
@@ -149,7 +149,7 @@ public class WordCount {
 		job.setReducerClass(CountReducer.class);
 		
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
 		
 		FileInputFormat.addInputPath(job, new Path(input));
 		FileOutputFormat.setOutputPath(job, new Path(output));
